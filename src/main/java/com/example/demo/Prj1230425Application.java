585446e66b44e7b2850fd64ef4de3a72af0f9389
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-25 16:43:15 +0900

    first

---
+++ src/main/java/com/example/demo/Prj1230425Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj1230425Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj1230425Application.class, args);
+	}
+
+}
