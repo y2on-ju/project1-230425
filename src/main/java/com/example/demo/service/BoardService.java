18e66b0230a0d754881317c3cf9e256c92ef954d
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-26 15:29:52 +0900

    Create BoardService.java

---
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+
+import com.example.demo.domain.*;
+import com.example.demo.mapper.*;
+
+@Service
+public class BoardService {
+
+	@Autowired
+	private BoardMapper mapper;
+
+	public List<Board> listBoard() {
+		List<Board> list = mapper.selectAll();
+		return list;
+	}
+
+	public Board getBoard(Integer id) {
+		return mapper.selectById(id);
+	}
+
+	public boolean modify(Board board) {
+		int cnt = mapper.update(board);
+		
+		return cnt == 1;
+	}
+
+	public boolean remove(Integer id) {
+		int cnt = mapper.deleteById(id);
+		return cnt == 1;
+	}
+}
+
+
+
+
+
+
+
