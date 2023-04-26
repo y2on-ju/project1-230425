817d492707889a4896a43572d21d51cbd6de8a81
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-26 15:29:55 +0900

    Create BoardMapper.java

---
+++ src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -0,0 +1,52 @@
+package com.example.demo.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
+import com.example.demo.domain.*;
+
+@Mapper
+public interface BoardMapper {
+
+	@Select("""
+			SELECT
+				id,
+				title,
+				writer,
+				inserted
+			FROM Board
+			ORDER BY id DESC
+			""")
+	List<Board> selectAll();
+
+	@Select("""
+			SELECT *
+			FROM Board
+			WHERE id = #{id}
+			""")
+	Board selectById(Integer id);
+
+	@Update("""
+			UPDATE Board
+			SET 
+				title = #{title},
+				body = #{body},
+				writer = #{writer}
+			WHERE
+				id = #{id}
+			""")
+	int update(Board board);
+
+	@Delete("""
+			DELETE FROM Board
+			WHERE id = #{id}
+			""")
+	int deleteById(Integer id);
+
+	
+}
+
+
+
+
