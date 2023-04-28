5389d65ba68aa4b29cd7dde5d2135c507b7f48cb
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-04-28 17:36:57 +0900

    Update BoardService.java

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -39,6 +39,41 @@ public class BoardService {
 //		int cnt = 0; // 실패
 		return cnt == 1;
 	}
+
+	public Map<String, Object> listBoard(Integer page, String search) {
+		// 페이지당 행의 수
+		Integer rowPerPage = 15;
+		
+		// 쿼리 LIMIT 절에 사용할 시작 인덱스
+		Integer startIndex = (page - 1) * rowPerPage;
+		
+		// 페이지네이션이 필요한 정보
+		// 전체 레코드 수
+		Integer numOfRecords = mapper.countAll(search);
+		// 마지막 페이지 번호
+		Integer lastPageNumber = (numOfRecords - 1) / rowPerPage + 1;
+		// 페이지네이션 왼쪽번호
+		Integer leftPageNum = page - 5;
+		// 1보다 작을 수 없음
+		leftPageNum = Math.max(leftPageNum, 1);
+		
+		// 페이지네이션 오른쪽번호
+		Integer rightPageNum = leftPageNum + 9;
+		// 마지막페이지보다 클 수 없음
+		rightPageNum = Math.min(rightPageNum, lastPageNumber);
+		
+		Map<String, Object> pageInfo = new HashMap<>();
+		pageInfo.put("rightPageNum", rightPageNum);
+		pageInfo.put("leftPageNum", leftPageNum);
+		pageInfo.put("currentPageNum", page);
+		pageInfo.put("lastPageNum", lastPageNumber);
+		
+		// 게시물 목록
+		List<Board> list = mapper.selectAllPaging(startIndex, rowPerPage, search);
+		
+		return Map.of("pageInfo", pageInfo, 
+				      "boardList", list);
+	}
 }
 
 
