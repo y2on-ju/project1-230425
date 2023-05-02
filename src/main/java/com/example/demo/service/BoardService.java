638b77b458a55b0ea72f0aa1727210b8ada49c4c
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-02 20:43:30 +0900

    Update BoardService.java

--- src/main/java/com/example/demo/service/BoardService.java
+++ src/main/java/com/example/demo/service/BoardService.java
@@ -1,14 +1,18 @@
 package com.example.demo.service;
 
+import java.io.*;
 import java.util.*;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+import org.springframework.web.multipart.*;
 
 import com.example.demo.domain.*;
 import com.example.demo.mapper.*;
 
 @Service
+@Transactional(rollbackFor = Exception.class)
 public class BoardService {
 
 	@Autowired
@@ -25,7 +29,7 @@ public class BoardService {
 
 	public boolean modify(Board board) {
 		int cnt = mapper.update(board);
-		
+
 		return cnt == 1;
 	}
 
@@ -34,20 +38,41 @@ public class BoardService {
 		return cnt == 1;
 	}
 
-	public boolean addBoard(Board board) {
+	public boolean addBoard(Board board, MultipartFile[] files) throws Exception {
+
+		// 게시물 insert
 		int cnt = mapper.insert(board);
-//		int cnt = 0; // 실패
+
+		for (MultipartFile file : files) {
+			if (file.getSize() > 0) {
+				System.out.println(file.getOriginalFilename());
+				System.out.println(file.getSize());
+				// 파일 저장 (파일 시스템에)
+				// 폴더 만들기
+				String folder = "C:\\study\\upload\\" + board.getId();
+				File targetFolder = new File(folder);
+				if (!targetFolder.exists()) {
+					targetFolder.mkdirs();
+				}
+
+				String path = folder + "\\" + file.getOriginalFilename();
+				File target = new File(path);
+				file.transferTo(target);
+				// db에 관련 정보 저장(insert)
+				mapper.insertFileName(board.getId(), file.getOriginalFilename());
+			}
+		}
+
 		return cnt == 1;
 	}
 
-	public Map<String, Object> listBoard
-			(Integer page, String search, String type) {
+	public Map<String, Object> listBoard(Integer page, String search, String type) {
 		// 페이지당 행의 수
 		Integer rowPerPage = 15;
-		
+
 		// 쿼리 LIMIT 절에 사용할 시작 인덱스
 		Integer startIndex = (page - 1) * rowPerPage;
-		
+
 		// 페이지네이션이 필요한 정보
 		// 전체 레코드 수
 		Integer numOfRecords = mapper.countAll(search, type);
@@ -57,29 +82,22 @@ public class BoardService {
 		Integer leftPageNum = page - 5;
 		// 1보다 작을 수 없음
 		leftPageNum = Math.max(leftPageNum, 1);
-		
+
 		// 페이지네이션 오른쪽번호
 		Integer rightPageNum = leftPageNum + 9;
 		// 마지막페이지보다 클 수 없음
 		rightPageNum = Math.min(rightPageNum, lastPageNumber);
-		
+
 		Map<String, Object> pageInfo = new HashMap<>();
 		pageInfo.put("rightPageNum", rightPageNum);
 		pageInfo.put("leftPageNum", leftPageNum);
 		pageInfo.put("currentPageNum", page);
 		pageInfo.put("lastPageNum", lastPageNumber);
-		
+
 		// 게시물 목록
 		List<Board> list = mapper.selectAllPaging(startIndex, rowPerPage, search, type);
-		
-		return Map.of("pageInfo", pageInfo, 
-				      "boardList", list);
+
+		return Map.of("pageInfo", pageInfo,
+				"boardList", list);
 	}
 }
-
-
-
-
-
-
-
