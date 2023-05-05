a040bcbed1cd94a6d0d2bd7e53484f68d574c547
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-05 09:58:08 +0900

    Update BoardMapper.java

--- src/main/java/com/example/demo/mapper/BoardMapper.java
+++ src/main/java/com/example/demo/mapper/BoardMapper.java
@@ -115,6 +115,26 @@ public interface BoardMapper {
 			VALUES (#{boardId}, #{fileName})
 			""")
 	Integer insertFileName(Integer boardId, String fileName);
+
+	@Select("""
+			SELECT fileName FROM FileName
+			WHERE boardId = #{boardId}
+			""")
+	List<String> selectFileNamesByBoardId(Integer boardId);
+
+	@Delete("""
+			DELETE FROM FileName 
+			WHERE boardId = #{boardId}
+			""")
+	void deleteFileNameByBoardId(Integer boardId);
+
+	@Delete("""
+			DELETE FROM FileName
+			WHERE 	boardId = #{boardId} 
+				AND fileName = #{fileName}
+			""")
+
+	void deleteFileNameByBoardIdAndFileName(Integer boardId, String fileName);
 	
 
 	
