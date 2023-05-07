8f705f8d44daf79d709082087019399f587578f6
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 00:24:05 +0900

    Create MemberService.java

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import java.util.*;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+
+import com.example.demo.domain.*;
+import com.example.demo.mapper.*;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class MemberService {
+
+	@Autowired
+	private MemberMapper mapper;
+
+	public boolean signup(Member member) {
+		int cnt = mapper.insert(member);
+		return cnt == 1;
+	}
+
+	public List<Member> listMember() {
+		
+		return mapper.selectAll();
+		
+	}
+
+	public Member get(String id) {
+		return mapper.selectById(id);
+	}
+}
+
+
+
