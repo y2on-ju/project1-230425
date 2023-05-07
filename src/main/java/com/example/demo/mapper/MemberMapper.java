6e85adc6a5ce3f899b0a430dce937dfac668b625
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 00:24:11 +0900

    Create MemberMapper.java

---
+++ src/main/java/com/example/demo/mapper/MemberMapper.java
@@ -0,0 +1,36 @@
+package com.example.demo.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
+import com.example.demo.domain.*;
+
+@Mapper
+public interface MemberMapper {
+
+	@Insert("""
+			INSERT INTO Member (id, password, nickName, email)
+			VALUES (#{id}, #{password}, #{nickName}, #{email})
+			""")
+	int insert(Member member);
+
+	@Select("""
+			SELECT *
+			FROM Member
+			ORDER BY inserted DESC
+			""")
+	List<Member> selectAll();
+
+	@Select("""
+			SELECT *
+			FROM Member
+			WHERE id = #{id}
+			""")
+	Member selectById(String id);
+}
+
+
+
+
+
