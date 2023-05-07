8f8da9ece11009847d0c2d2e8103449aa6337b15
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 00:24:15 +0900

    Create Member.java

---
+++ src/main/java/com/example/demo/domain/Member.java
@@ -0,0 +1,19 @@
+package com.example.demo.domain;
+
+import java.time.*;
+
+import lombok.*;
+
+@Data
+public class Member {
+	private String id;
+	private String password;
+	private String email;
+	private String nickName;
+	private LocalDateTime inserted;
+}
+
+
+
+
+
