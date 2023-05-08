205b0fa201fe35bd7b9cd82bf9ed338b55f4be1d
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 17:44:44 +0900

    Update MemberMapper.java

--- src/main/java/com/example/demo/mapper/MemberMapper.java
+++ src/main/java/com/example/demo/mapper/MemberMapper.java
@@ -28,6 +28,30 @@ public interface MemberMapper {
 			WHERE id = #{id}
 			""")
 	Member selectById(String id);
+
+	@Delete("""
+			DELETE FROM Member
+			WHERE id = #{id}
+			""")
+	Integer deleteById(String id);
+
+	@Update("""
+			<script>
+			
+			UPDATE Member
+			SET 
+				<if test="password neq null and password neq ''">
+				password = #{password},
+				</if>
+				
+			    nickName = #{nickName},
+			    email = #{email}
+			WHERE
+				id = #{id}
+			
+			</script>
+			""")
+	Integer update(Member member);
 }
 
 
