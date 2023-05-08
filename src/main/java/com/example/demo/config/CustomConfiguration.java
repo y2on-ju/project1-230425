10da1e2014919b45253fcef5aeddf9d040b6d7cc
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-08 17:44:49 +0900

    Update CustomConfiguration.java

--- src/main/java/com/example/demo/config/CustomConfiguration.java
+++ src/main/java/com/example/demo/config/CustomConfiguration.java
@@ -2,12 +2,16 @@ package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.*;
+import org.springframework.security.crypto.bcrypt.*;
+import org.springframework.security.crypto.password.*;
+import org.springframework.security.web.*;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.servlet.ServletContext;
+import jakarta.annotation.*;
+import jakarta.servlet.*;
 import software.amazon.awssdk.auth.credentials.*;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.regions.*;
+import software.amazon.awssdk.services.s3.*;
 
 @Configuration
 public class CustomConfiguration {
@@ -19,7 +23,7 @@ public class CustomConfiguration {
 	
 	@Value("${aws.bucketUrl}")
 	private String bucketUrl;
-
+	
 	@Autowired
 	private ServletContext application;
 	
@@ -28,6 +32,18 @@ public class CustomConfiguration {
 		application.setAttribute("bucketUrl", bucketUrl);
 	}
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		return http.build();
+	}
+	
+
 	@Bean
 	public S3Client s3client() {
 		
