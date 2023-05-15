c2836d7c2c53c6038bf04da9fe423cd067c01729
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-15 16:55:36 +0900

    Create like.js

---
+++ src/main/webapp/js/board/like.js
@@ -0,0 +1,26 @@
+$("#likeIcon").click(function() {
+	// 게시물 번호 request body에 추가
+	const boardId = $("#boardIdText").text().trim();
+	// const data = {boardId : boardId};
+	const data = {boardId};
+	
+	$.ajax("/like", {
+		method: "post",
+		contentType: "application/json",
+		data: JSON.stringify(data),
+		
+		success: function(data) {
+			if (data.like) {
+				// 꽉찬 하트
+				$("#likeIcon").html(`<i class="fa-solid fa-heart"></i>`);
+			} else {
+				// 빈 하트
+				$("#likeIcon").html(`<i class="fa-regular fa-heart"></i>`);
+			}
+		}
+		//error:,
+		//complete:,
+	});
+});
+
+
