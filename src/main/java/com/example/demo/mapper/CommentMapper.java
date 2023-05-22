7a3933e952b4c60ec879744dd90a4d8fff22ce9e
y2on-ju <ksyj8997@naver.com>
2023-05-22 21:02:58 +0900

    Create CommentMapper.java

---
+++ src/main/java/com/example/demo/mapper/CommentMapper.java
@@ -0,0 +1,50 @@
+package com.example.demo.mapper;
+
+import java.util.*;
+
+import org.apache.ibatis.annotations.*;
+
+import com.example.demo.domain.*;
+
+@Mapper
+public interface CommentMapper {
+
+	@Select("""
+			SELECT * 
+			FROM Comment
+			WHERE boardId = #{boardId}
+			ORDER BY id
+			""")
+	List<Comment> selectAllByBoardId(Integer boardId);
+
+	@Insert("""
+			INSERT INTO Comment (boardId, content, memberId)
+			VALUES (#{boardId}, #{content}, #{memberId})
+			""")
+	Integer insert(Comment comment);
+
+	@Delete("""
+			DELETE FROM Comment
+			WHERE id = #{id}
+			""")
+	Integer deleteById(Integer id);
+
+	@Select("""
+			SELECT *
+			FROM Comment
+			WHERE id = #{id}
+			""")
+	Comment selectById(Integer id);
+
+	@Update("""
+			UPDATE Comment
+			SET
+				content = #{content}
+			WHERE
+				id = #{id}
+			""")
+	Integer update(Comment comment);
+	
+}
+
+
